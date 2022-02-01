2f245f204c17940a68078f80b5bbcdb8d9a4337d
charlieIT <>
2022-02-01 19:45:41 +0000

    Updated series!; Generalized axis length and type validations Added generic set_axis! and append_axis! for defining and validating axis Added dispatch on Axis and SeriesOptions Initialized empty axis, when not defined

--- src/ECharts/Series.jl
+++ src/ECharts/Series.jl
@@ -1,51 +1,68 @@
-function series!(ec::EChart,kind::String,args...;kwargs...)
-    data=[]
-    for r in args
-       push!(data,DataAttrs(r))
-    end
+function set_axis!(ec::EChart, options::SeriesOptions)
     
-    series_options=Dict()
-    series_name="Series"*string(length(ec["series"])+1)
+    function append_axis!(ec::EChart, axis::Type{A}, axis_value) where A<:Axis
+        
+        if length(ec[axis]) == 2
+            existing_axis_types = map(x->x["type"], ec[axis])
+            @assert axis_value["type"] in existing_axis_types """$(string(axis)) type must be one of the two already existent: $(join(existing_axis_types, ", "))..."""
+        end
+        
+        # no axis defined, or new axis type differs from previously added axis
+        if isempty(ec[axis]) ||
+            all(x->x["type"] != axis_value["type"], ec[axis])
+            
+            push!(ec.options[string(axis)], axis_value)
+            
+            # add [xAxis/yAxis]Index key pointing to axis position, key "xAxisIndex" or "yAxisIndex"
+            ec["series"][end][string(string(Axis), "Index")] = length(ec[axis])
+        end
+    end
+    [append_axis!(ec, axis, Base.getproperty(options, axis)) for axis in [xAxis, yAxis]]
+end
+
+"""
+    series!(ec::EChart, kind::String, args...; kwargs...)
+
+Examples
+
+```julia
+series!(mychart, "bar", ["A","B","C"], [1,2,3], name="My Series")
+```
+"""
+function series!(ec::EChart, kind::String, args...; kwargs...)
+    data = Vector{DataAttrs}([DataAttrs(arg) for arg in args])
+    
+    series_options = Dict()
+    series_name = string("Series", string(length(ec["series"]) + 1))
     
     for (k,v) in kwargs
-       k=string(k)
-       if k=="name"
-          series_name=v
+       k = string(k)
+       if k == "name"
+          series_name = v
        else
-         series_options[k]=v 
+         series_options[k] = v 
        end
     end
     
-    nt=Namtso.dict(Series(kind,series_name,data,series_options))
-    
-    push!(ec["series"],nt.series_options)
-    len_x=length(ec.options["xAxis"])
-    len_y=length(ec.options["yAxis"])
-        
-    if len_x==1
-        if ec.options["xAxis"][1]["type"]!=nt.xaxis["type"]
-            push!(ec.options["xAxis"],nt.xaxis)
-            ec["series"][end]["xAxisIndex"]=1
-        end
-    elseif len_x==2
-        @assert nt.xaxis["type"] in map(x->x["type"],ec.options["xAxis"]) "X Axis type must be one of the two existent"
+    # init empty axis if not present
+    if !haskey(ec.options, "xAxis")
+        ec.options["xAxis"] = []
+    end
+    if !haskey(ec.options, "yAxis")
+        ec.options["yAxis"] = []
     end
     
-    if len_y==1
-        if ec.options["yAxis"][1]["type"]!=nt.yaxis["type"]
-            push!(ec.options["yAxis"],nt.yaxis)
-            ec["series"][end]["yAxisIndex"]=1
-        end
-    elseif len_y==2
-        @assert nt.yaxis["type"] in map(x->x["type"],ec.options["yAxis"]) "Y Axis type must be one of the two existent"
-    end
+    nt = Options(Series(kind, series_name, data, series_options))
+    push!(ec["series"], nt.options)
+    
+    # set axis according to SeriesOptions
+    set_axis!(ec, nt)
     
     ## legend
-    if length(ec["series"])>1
-       ec.options["legend"]=Dict("data"=>map(x->x["name"],ec["series"])) 
+    if length(ec["series"]) > 1
+       ec.options["legend"] = Dict("data"=>map(x->x["name"],ec["series"])) 
     end
         
-    
-    ec.options=dict_any(ec.options)
-    return nothing
+    ec.options = dict_any(ec.options)
+    return;
 end
