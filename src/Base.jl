2e6ab6636c598b602d1e05046768909990f20d4a
charlieIT <>
2022-02-01 19:38:37 +0000

    Updated Base; Updated Series and Plot constructors; Added OptionTypes, SeriesData; Renamed axisTypes; Added default series constructor; Added empty Plot constructor; Moved method dict_any from dict.jl Added docstrings

--- src/Base.jl
+++ src/Base.jl
@@ -1,16 +1,61 @@
-axisTypes=Union{Number,Date,AbstractString}
+const OptionTypes = Dict{String, Any}
+const AxisTypes   = Union{Number, Date, AbstractString}
+
+"""
+    DataAttrs
+"""
 struct DataAttrs
-    data::Vector{T} where T<:axisTypes
+    data::Vector{T} where T<:AxisTypes
 end
 
+const SeriesData = Vector{DataAttrs}
+
+"""
+    Series
+"""
 mutable struct Series
-   plot_type::String
-   name::String
-   data::Vector{DataAttrs} 
-   options::Dict{String,Any} 
+    plot_type::String
+    name::String
+    data::SeriesData
+    options::OptionTypes
 end
 
+"""
+    Series(kind::String; name, kwargs...)
+
+Define a Series object with empty `data`
+
+Data and naming for this type of series will be provided through other charts definitions, e.g. `dataset`
+"""
+function Series(kind::String; name::String="", kwargs...)
+    return Series(kind, name, Vector{DataAttrs}(), Dict{String, Any}(kwargs...))
+end
+
+"""
+    Plot
+"""
 mutable struct Plot
-   series::Vector{Series} 
-   options::Dict{String,Any} 
+    series::Vector{Series} 
+    options::OptionTypes
+    
+    function Plot()
+        return new(Vector{Series}(), Dict{String, Any}())
+    end
+    function Plot(series::Vector{Series}, options::Dict)
+        return new(series, dict_any(options))
+    end
+end
+
+"""
+    dict_any(d::Dict)
+
+Convert an input dictionary to `Dict{String, Any}`
+Also recursively convert all dict values to `Dict{String, Any}` 
+"""
+function dict_any(d::Dict)
+    d = convert(Dict{String, Any}, d)
+    for (k,v) in d
+        if v isa Dict d[k] = dict_any(v) end
+    end
+    return d
 end
