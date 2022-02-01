d3c5e8f95b1bf3f4930b454e5be371b842be8cca
charlieIT <>
2022-02-01 19:48:06 +0000

    Updated Module Updated exports Added Tools Added PUBLIC_SCRIPT

--- src/Namtso.jl
+++ src/Namtso.jl
@@ -1,10 +1,13 @@
 module Namtso
 
-using Dates,JSON,Random
+using Dates, JSON, Random
 
-export EChart,series!,public_render!
+export EChart, series!
+export Tools
 
+PUBLIC_SCRIPT = "https://cdnjs.cloudflare.com/ajax/libs/echarts/4.6.0/echarts.min.js"
 include("Base.jl")
 include("ECharts/ECharts.jl")
+include("Tools/BoxPlot.jl")
 
 end # module
