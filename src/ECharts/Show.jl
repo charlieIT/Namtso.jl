71be8d97cb3f25d389213d5b61796f6ad8442147
charlieIT <>
2022-02-01 19:47:16 +0000

    Updated display logic Added support for non-px grid dimensions Added initial support for container reactivity Moved script url to constant Added renderer capabilities

--- src/ECharts/Show.jl
+++ src/ECharts/Show.jl
@@ -1,18 +1,29 @@
-import Base.show
+function str(dimension::GridDimension)
+    dim = "$dimension"
+    return dimension isa Int64 ? string(dim, "px") : dim
+end
 
+import Base.show
 function Base.show(io::IO, mm::MIME"text/html", ec::EChart)
 
-     id=ec.id*randstring(5)
-     options = Namtso.echart_json(ec.options)
+    id = ec.id*randstring(5) #????
+    options = Namtso.echart_json(ec.options)
+
+    renderer = JSON.json(ec.renderer)
     
-     dom_str="""
-        <div id=\"$(id)\" style=\"height:$(ec.height)px;width:$(ec.width)px;\"></div>
-        <script type=\"text/javascript\">
-                var myChart = echarts.init(document.getElementById(\"$(id)\"));
-                myChart.setOption($options);
-        </script>
-      """
-    public_script="""<script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/4.6.0/echarts.min.js"></script>"""
-    str=public_script*dom_str
-    println(io,str)
+    dom_str = """
+        <div id="$(id)" style="height:$(str(ec.height)); width:$(str(ec.width));"></div>
+        <script type="text/javascript">
+        var myChart = echarts.init(document.getElementById("$(id)"), null, $renderer);
+        myChart.setOption($options);
+        """
+    
+    if ec.resize 
+        domstr = string(dom_str, "window.onresize = function(){myChart.resize();};") 
+    end
+    
+    dom_str = string(dom_str, "</script>")
+    
+    public_script="""<script src="$(PUBLIC_SCRIPT)"></script>"""
+    println(io, string(public_script, dom_str))
 end
